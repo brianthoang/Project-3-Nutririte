8d0a89b339fec2df676f5d40ca245c99c42e1f5e
Kevin Ng <kevinng3292@gmail.com>
2022-12-12 20:18:39 -0800

    adjusting home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import Login from '../components/Login';
+import Register from '../components/Register';
+
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+// const Home = () => {
+//     const {}
+// }
